af5a115ba57d52e29a21d249deac1ed2ce38455f
XINGMOUREN <2638526782@qq.com>
2022-08-04 23:27:33 +0800

    更新天气api

--- js/main.js
+++ js/main.js
@@ -156,9 +156,9 @@ $('#hitokoto').click(function () {
 //获取天气
 //请前往 https://www.mxnzp.com/doc/list 申请 app_id 和 app_secret
 //请前往 https://dev.qweather.com/ 申请 key
-const add_id = "wrknltonr0foslhs"; // app_id
-const app_secret = "Nlh1c0F6d0ZDU2pDR0J3YVBVbkhudz09"; // app_secret
-const key = "433f0c48615a48dfaf2f2b2444297e79" // key
+const add_id = "kmzgkltvyxuveebk"; // app_id
+const app_secret = "aHQ3SnhVVUJDTzdZWFFCSld6bG83UT09"; // app_secret
+const key = "3de6e2f35d58446d9f7077bb358b9ca0" // key
 function getWeather() {
     fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
         .then(response => response.json())
