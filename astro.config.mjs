dbc80f43ced2d2a88ad20b54a2585500d6f7c458
XINGMOUREN <2638526782@qq.com>
2024-02-24 14:21:19 +0800

    提交源代码

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import {site} from "./src/config"
-
-
-// https://astro.build/config
-export default defineConfig({
-  site: site.url,
-  integrations: []
-});
